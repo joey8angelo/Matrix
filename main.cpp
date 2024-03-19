5ca25ed5070573ded12a3c26fd1cc374c234a5c6
joey8angelo <joey8angelo@gmail.com>
2024-03-19 15:46:14 -0700

    update DAC multiplication to be in-place

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 const std::size_t N = 3200;
 
 const int PAR_GRAN = 50;
-const int DAC_GRAN = 1000;
+const int DAC_GRAN = 50;
 
 using namespace parlay;
 
@@ -41,134 +41,91 @@ void add(const int* A, const int* B, int* C, std::size_t N, std::size_t M){
     }
 }
 
-void DACMult(const int* A, const int* B, int* C, std::size_t Na, std::size_t Ma, std::size_t Nb, std::size_t Mb){
-    if(std::min(Na, std::min(Ma, std::min(Nb, Mb))) <= DAC_GRAN) parallelMult(A,B,C,Na,Ma,Nb,Mb);
-    else {
-        std::size_t Nad = Na / 2;
-        std::size_t Mad = Ma / 2;
-        std::size_t Nbd = Nb / 2;
-        std::size_t Mbd = Mb / 2;
-        
-        int* B11 = new int[Nbd * Mbd];
-        int* B12 = new int[Nbd * (Mb - Mbd)];
-        int* B21 = new int[(Nb - Nbd) * Mbd];
-        int* B22 = new int[(Nb - Nbd) * (Mb - Mbd)];
-
-        for(std::size_t i = 0; i < Nbd; i++){
-            for(std::size_t j = 0; j < Mbd; j++){
-                B11[i * Mbd + j] = B[i * Mb + j];
-            }
-        }
-        for(std::size_t i = 0; i < Nbd; i++){
-            for(std::size_t j = 0; j < Mb - Mbd; j++){
-                B12[i * (Mb - Mbd) + j] = B[i * Mb + (j + Mbd)];
-            }
-        }
-        for(std::size_t i = 0; i < Nb - Nbd; i++){
-            for(std::size_t j = 0; j < Mbd; j++){
-                B21[i * Mbd + j] = B[((i + Nbd) * Mb) + j];
-            }
-        }
-        for(std::size_t i = 0; i < Nb - Nbd; i++){
-            for(std::size_t j = 0; j < Mb - Mbd; j++){
-                B22[i * (Mb - Mbd) + j] = B[(i + Nbd) * Mb + (j + Mbd)];
-            }
-        }
-
-        int* C11 = new int[Nad * Mbd];
-        int* C12 = new int[Nad * (Mb - Mbd)];
-        int* C21 = new int[(Na - Nad) * Mbd];
-        int* C22 = new int[(Na - Nad) * (Mb - Mbd)];
-
-        par_do([&](){
-            int* A11 = new int[Nad * Mad];
-            for(std::size_t i = 0; i < Nad; i++){
-                for(std::size_t j = 0; j < Mad; j++){
-                    A11[i * Mad + j] = A[i * Ma + j];
-                }
-            }
-            int* A12 = new int[Nad * (Ma - Mad)];
-            for(std::size_t i = 0; i < Nad; i++){
-                for(std::size_t j = 0; j < Ma - Mad; j++){
-                    A12[i * (Ma - Mad) + j] = A[i * Ma + (j + Mad)];
-                }
-            }
-            par_do([&](){
-                int* t1 = new int[Nad * Mbd];
-                int* t2 = new int[Nad * Mbd];
-                par_do([&](){
-                    DACMult(A11, B11, t1, Nad, Mad, Nbd, Mbd); 
-                }, [&](){ 
-                    DACMult(A12, B21, t2, Nad, Ma - Mad, Nb - Nbd, Mbd); 
-                });
-                add(t1, t2, C11, Nad, Mbd);
-            },[&](){
-                int* t1 = new int[Nad * (Mb - Mbd)];
-                int* t2 = new int[Nad * (Mb - Mbd)];
-                par_do([&](){ 
-                    DACMult(A11, B12, t1, Nad, Mad, Nbd, Mb - Mbd); 
-                }, [&](){ 
-                    DACMult(A12, B22, t2, Nad, Ma - Mad, Nb - Nbd, Mb - Mbd); 
-                });
-                add(t1, t2, C12, Nad, Mb - Mbd);
-            });
-        },[&](){
-            int* A21 = new int[(Na - Nad) * Mad];
-            for(std::size_t i = 0; i < Na - Nad; i++){
-                for(std::size_t j = 0; j < Mad; j++){
-                    A21[i * Mad + j] = A[(i + Nad) * Ma + j];
-                }
-            }
-            int* A22 = new int[(Na - Nad) * (Ma - Mad)];
-            for(std::size_t i = 0; i < Na - Nad; i++){
-                for(std::size_t j = 0; j < Ma - Mad; j++){
-                    A22[i * (Ma - Mad) + j] = A[(i + Nad) * Ma + (j + Mad)];
-                }
-            }
-            par_do([&](){
-                int* t1 = new int[(Na - Nad) * Mbd];
-                int* t2 = new int[(Na - Nad) * Mbd];
-                par_do([&](){ 
-                    DACMult(A21, B11, t1, Na - Nad, Mad, Nbd, Mbd); 
-                }, [&](){ 
-                    DACMult(A22, B21, t2, Na - Nad, Ma - Mad, Nb - Nbd, Mbd); 
-                });
-                add(t1, t2, C21, Na - Nad, Mbd);
-            },[&](){
-                int* t1 = new int[(Na - Nad) * (Mb - Mbd)];
-                int* t2 = new int[(Na - Nad) * (Mb - Mbd)];
-                par_do([&](){ 
-                    DACMult(A21, B12, t1, Na - Nad, Mad, Nbd, Mb - Mbd); 
-                }, [&](){ 
-                    DACMult(A22, B22, t2, Na - Nad, Ma - Mad, Nb - Nbd, Mb - Mbd); 
-                });
-                add(t1, t2, C22, Na - Nad, Mb - Mbd);
-            });
-        });
-
-        for(std::size_t i = 0; i < Nad; i++){
-            for(std::size_t j = 0; j < Mbd; j++){
-                C[i * Mb + j] = C11[i * Mbd + j];
-            }
-        }
-        for(std::size_t i = 0; i < Nad; i++){
-            for(std::size_t j = 0; j < Mb-Mbd; j++){
-                C[i * Mb + (j + Mbd)] = C12[i * (Mb-Mbd) + j];
-            }
-        }
-        for(std::size_t i = 0; i < Na - Nad; i++){
-            for(std::size_t j = 0; j < Mbd; j++){
-                C[(i + Nad) * Mb + j] = C21[i * Mbd + j];
-            }
-        }
-        for(std::size_t i = 0; i < Na - Nad; i++){
-            for(std::size_t j = 0; j < Mb - Mbd; j++){
-                C[(i + Nad) * Mb + (j + Mbd)] = C22[i * (Mb - Mbd) + j];
+void multAt(const int* A, const int* B, int* C, std::size_t Na, std::size_t Ma, std::size_t Nb, std::size_t Mb, std::size_t PaN, std::size_t PaM, std::size_t PbN, std::size_t PbM, std::size_t PcN, std::size_t PcM){
+    for(std::size_t i = 0; i < Na; i++){
+        for(std::size_t k = 0; k < Ma; k++){
+            for(std::size_t j = 0; j < Mb; j++){
+                C[(i+PcN) * N + (j+PcM)] += A[(i+PaN) * N + (k+PaM)] * B[(k+PbN) * N + (j+PbM)];
             }
         }
     }
 }
 
+void DACMult(const int* A, const int* B, int* C, std::size_t Na, std::size_t Ma, std::size_t Nb, std::size_t Mb, std::size_t PaN, std::size_t PaM, std::size_t PbN, std::size_t PbM, std::size_t PcN, std::size_t PcM){
+    if(std::min(Na, std::min(Ma, std::min(Nb, Mb))) <= DAC_GRAN) multAt(A,B,C,Na,Ma,Nb,Mb,PaN,PaM,PbN,PbM,PcN,PcM);
+    else {
+        std::size_t Nad = Na / 2;
+        std::size_t Mad = Ma / 2;
+        std::size_t Nbd = Nb / 2;
+        std::size_t Mbd = Mb / 2;
+
+        std::size_t A11N = PaN;
+        std::size_t A11M = PaM;
+        std::size_t A12N = PaN;
+        std::size_t A12M = PaM + Mad;
+        std::size_t A21N = PaN + Nad;
+        std::size_t A21M = PaM;
+        std::size_t A22N = PaN + Nad;
+        std::size_t A22M = PaM + Mad;
+
+        std::size_t B11N = PbN;
+        std::size_t B11M = PbM;
+        std::size_t B12N = PbN;
+        std::size_t B12M = PbM + Mbd;
+        std::size_t B21N = PbN + Nbd;
+        std::size_t B21M = PbM;
+        std::size_t B22N = PbN + Nbd;
+        std::size_t B22M = PbM + Mbd;
+
+        std::size_t C11N = PcN;
+        std::size_t C11M = PcM;
+        std::size_t C12N = PcN;
+        std::size_t C12M = PcM + Mbd;
+        std::size_t C21N = PcN + Nad;
+        std::size_t C21M = PcM;
+        std::size_t C22N = PcN + Nad;
+        std::size_t C22M = PcM + Mbd;
+
+        par_do([&](){
+            par_do([&](){
+                par_do([&](){
+                    DACMult(A, B, C, Nad, Mad     , Nbd     , Mbd, A11N, A11M, B11N, B11M, C11N, C11M); // indexing A11, B11, C11
+                },[&](){
+                    DACMult(A, B, C, Nad, Mad     ,      Nbd, Mb - Mbd, A11N, A11M, B12N, B12M, C12N, C12M); // indexing A11, B12, C12
+                });
+            },[&](){
+                par_do([&](){
+                    DACMult(A, B, C, Na - Nad, Mad     , Nbd     , Mbd, A21N, A21M, B11N, B11M, C21N, C21M); // indexing A21, B11, C21
+                },[&](){
+                    DACMult(A, B, C, Na - Nad, Mad     , Nbd     , Mb - Mbd, A21N, A21M, B12N, B12M, C22N, C22M); // indexing A21, B12, C22
+                });
+            });
+        },[&](){
+            par_do([&](){
+                par_do([&](){
+                    DACMult(A, B, C, Nad, Ma - Mad, Nb - Nbd, Mbd, A12N, A12M, B21N, B21M, C11N, C11M); // indexing A12, B21, C11
+                },[&](){
+                    DACMult(A, B, C, Nad, Ma - Mad, Nb - Nbd, Mb - Mbd, A12N, A12M, B22N, B22M, C12N, C12M); // indexing A12, B22, C12
+                });
+            },[&](){
+                par_do([&](){
+                    DACMult(A, B, C, Na - Nad, Ma - Mad, Nb - Nbd, Mbd, A22N, A22M, B21N, B21M, C21N, C21M); // indexing A22, B21, C21
+                },[&](){
+                    DACMult(A, B, C, Na - Nad, Ma - Mad, Nb - Nbd, Mb - Mbd, A22N, A22M, B22N, B22M, C22N, C22M); // indexing A22, B22, C22
+                });
+            });
+        });
+
+    }
+}
+
+void zeroOut(int* A, std::size_t N, std::size_t M){
+    for(std::size_t i = 0; i < N; i++){
+        for(std::size_t j = 0; j < M; j++){
+            A[i * M + j] = 0;
+        }
+    }
+}
 
 int main(){
     int* A = new int[N*N];
@@ -179,10 +136,19 @@ int main(){
         for(std::size_t j = 0; j < N; j++){
             A[i*N + j] = rand() % 10;
             B[i*N + j] = rand() % 10;
+            C[i*N + j] = 0;
         }
     }
 
     std::vector<double> times(3);
+
+    DACMult(A, B, C, N, N, N, N, 0, 0, 0, 0, 0, 0);
+    for(std::size_t i = 0; i < N; i++){
+        for(std::size_t j = 0; j < N; j++){
+            std::cout << C[i*N + j] << " ";
+        }
+        std::cout << std::endl;
+    }
 
     for(int i = 0; i < 4; i++){
         std::cout << "Round " << i + 1 << std::endl;
@@ -190,14 +156,17 @@ int main(){
         sequentialMult(A, B, C, N, N, N, N);
         times[0] += std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start).count();
         std::cout << "finished sequential" << std::endl;
+        zeroOut(C, N, N);
         start = std::chrono::high_resolution_clock::now();
         parallelMult(A, B, C, N, N, N, N);
         times[1] += std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start).count();
         std::cout << "finished parallel" << std::endl;
+        zeroOut(C, N, N);
         start = std::chrono::high_resolution_clock::now();
-        DACMult(A, B, C, N, N, N, N);
+        DACMult(A, B, C, N, N, N, N, 0, 0, 0, 0, 0, 0);
         times[2] += std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start).count();
-        std::cout << "finished DAC" << std::endl;
+        std::cout << "finished DAC" << std::endl << std::endl;
+        zeroOut(C, N, N);
     }
     std::cout << "Sequential: " << times[0] / 4 << "ms" << std::endl;
     std::cout << "Parallel: " << times[1] / 4 << "ms" << std::endl;
